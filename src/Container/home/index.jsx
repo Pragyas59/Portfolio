3f7797cbc02d73f772d288802d65e005c50d9079
Pragyas59 <pragyas9525@gmail.com>
2024-04-07 10:18:59 +0530

    update

--- src/Container/home/index.jsx
+++ src/Container/home/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './style.css';
 import { useNavigate } from "react-router-dom";
-import myImage from './image.png';
+import myImage from './pragya.jpg';
 import {Animate} from 'react-simple-animate'
 
 const Home = () => {
